728e9a76f83d74f4b4e178b9fc97b49ba05d4e1b
Gim Beom Uk <marine0436@gmail.com>
2019-04-01 18:16:24 +0900

    コミットです。
    こみっと？？

--- author.php
+++ author.php
@@ -66,7 +66,7 @@
   </head>
   <body>
     <h1><a href = "index.php">WEB</a></h1>
-    <p><a href="index.php">topic</a></p>
+    <p><a href="index.php">topic！</a></p>
     <table border="1">
       <tr>
         <td>id</td><td>name</td><td>profile</td><td>update</td><td>delete</td>
